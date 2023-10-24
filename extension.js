ee6a45f5592df450e416e91e3c77762fa39013c5
aaron.xiao <admin@veryos.com>
2023-10-24 14:30:21 +0800

    feat(): move cache to home dir

--- extension.js
+++ extension.js
@@ -3,19 +3,23 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 const vscode = require('vscode');
 const spawn = require('cross-spawn');
-
 const git = require('./lib/git');
 const { window, commands, workspace, Uri } = vscode;
 
 let inProgress = false;
+let store = null;
+const ONE_HOUR = 3600000;
 const cachePath = '.devpack';
 const cachedDirs = new Map();
 const cachedRepos = new Map();
 const cachedGits = new Map();
 
 function activate(context) {
-  context.subscriptions.push(commands.registerCommand('devpack.QAKit.reload', checkAndInstall));
-  checkAndInstall();
+  store = new XStorage(context);
+  context.subscriptions.push(
+    commands.registerCommand('devpack.QAKit.reload', () => checkAndInstall(true))
+  );
+  checkAndInstall(true);
 
   // // check workspace folders and editors
   // if (window.visibleTextEditors) {
@@ -46,11 +50,18 @@ function activate(context) {
 }
 
 function deactivate() {
+  inProgress = false;
   cachedDirs.clear();
   cachedRepos.clear();
   cachedGits.clear();
+  const npmDir = getNpmDir();
+  const cacheDir = path.join(npmDir, cachePath);
+  if (fs.existsSync(cacheDir)) {
+    fs.rmdirSync(cacheDir);
+  }
 }
 
+// eslint-disable-next-line
 function startupFile(uri) {
   const dir = path.dirname(uri.path);
   startupFolder(uri, dir);
@@ -75,6 +86,7 @@ function startupFolder(uri, dir) {
   checkAndInstall(repo);
 }
 
+// eslint-disable-next-line
 function startupWorkspace(uri) {
   if (uri.scheme !== 'file') return;
 
@@ -113,14 +125,20 @@ function isCompanyRepo(repo) {
   return flag;
 }
 
-function checkAndInstall() {
+async function checkAndInstall(force) {
+  const now = Date.now();
+  const lastCheck = store.get('lastCheck');
   if (inProgress) return;
+  if (!force && lastCheck && now - lastCheck < ONE_HOUR) return;
   inProgress = true;
-  const npmDir = getNpmDir();
-  const cacheDir = path.join(npmDir, cachePath);
+  await store.set('lastCheck', now);
+  const cacheDir = getCacheDir();
   const localVer = getInstalled('devpack-qa', cacheDir);
   if (!localVer || getLatest('@devpack/qakit') !== localVer) {
     installModule(cacheDir);
+  } else {
+    inProgress = false;
+    window.setStatusBarMessage('QAKit Ready');
   }
 }
 
@@ -142,8 +160,9 @@ function getInstalled(name, cacheDir) {
     const out = spawn.sync('npx', ['--no-install', name, '-v'], {
       env: {
         ...process.env,
-        PATH: `${process.env.PATH}:${cacheDir}/node_modules/.bin`
+        PATH: `${cacheDir}/node_modules/.bin:${process.env.PATH}`
       },
+      shell: true,
       encoding: 'utf8',
       windowsHide: true
     });
@@ -167,6 +186,22 @@ function getLatest(pkg) {
   }
 }
 
+function getCacheDir() {
+  try {
+    const out = spawn.sync('echo', [`$HOME/${cachePath}`], {
+      shell: true,
+      encoding: 'utf8',
+      windowsHide: true
+    });
+    if (!out.status) {
+      return out.stdout.toString().trim();
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  return null;
+}
+
 function getNpmDir() {
   try {
     const out = spawn.sync('npm', ['config', 'get', 'prefix'], {
@@ -180,6 +215,23 @@ function getNpmDir() {
     console.error(err);
   }
   return null;
+}
+
+class XStorage {
+  constructor(context) {
+    this.context = context;
+  }
+  get(key) {
+    return this.context.globalState.get(key);
+  }
+  async set(key, val) {
+    await this.context.globalState.update(key, val);
+    return val;
+  }
+  async clear(key) {
+    await this.context.globalState.update(key, undefined);
+    return undefined;
+  }
 }
 
 module.exports = {
